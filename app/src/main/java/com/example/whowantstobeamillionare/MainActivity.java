32e1d1e6dd32af0845403c360f9f46229b8cb1ac
Benedikt Neubauer <benni.neubauer@gmx.net>
2020-03-10 15:50:19 +0000

    Initial commit

---
+++ app/src/main/java/com/example/whowantstobeamillionare/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.whowantstobeamillionare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.startscreen);
+
+        Button button1 = (Button) findViewById(R.id.buttonStart);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activityStart = new Intent(MainActivity.this,WhoWantsToBeAMillionare.class);
+                startActivityForResult(activityStart,1);
+            }
+        });
+      Button button2 = (Button) findViewById(R.id.buttonLoad);
+      button2.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              Toast.makeText(getBaseContext(),"Need to load last Game!",Toast.LENGTH_LONG).show();
+          }
+      });
+
+      Button button3 = (Button) findViewById(R.id.buttonOptions);
+      button3.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              Intent activityOptions = new Intent(MainActivity.this, Options.class);
+              startActivityForResult(activityOptions,1);
+          }
+      });
+
+      Button button4 = (Button) findViewById(R.id.buttonQuit);
+      button4.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              finish();
+              System.exit(0);
+          }
+      });
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/whowantstobeamillionare/Options.java
@@ -0,0 +1,11 @@
+package com.example.whowantstobeamillionare;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Options extends Activity {
+    public void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.setting_screen);
+    }
+}
